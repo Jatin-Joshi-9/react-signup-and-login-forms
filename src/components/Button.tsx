0c9ab60b9a372f3e8b7fa036da8f3f7674f3b08b
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-25 15:56:28 +0530

    feat: add Buttom component

---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+type ButtonProps = {
+    type: "submit" | "reset" | "button" | undefined;
+    label: string;
+}
+
+const Button = ({ type, label }: ButtonProps) => {
+    return (
+        <button
+            type={type}
+            className="w-full py-2.5 px-4 bg-sky-700 hover:bg-sky-800 text-white font-semibold rounded-lg transition-colors duration-200 cursor-pointer"
+        >
+            {label}
+        </button>
+    )
+}
+
+export default Button;
