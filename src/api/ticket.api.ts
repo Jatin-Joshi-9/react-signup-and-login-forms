213971a5fddedfa8467870829d313620e30c5780
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-27 15:19:24 +0530

    refactor: move CreateTicket to pages and ticket api call service to api folder

---
+++ src/api/ticket.api.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import type { TicketType } from "../types/ticket";
+import { getAuthToken } from "../utils/authToken";
+
+const baseURI: string = import.meta.env.VITE_TICKET_URI;
+
+export const createTicket = async (values: TicketType) => {
+    const token = getAuthToken();
+    if (!token) {
+        return null;
+    }
+    const response = await axios.post(`${baseURI}`, values, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        }
+    });
+    return response.data;
+}
+
+export const getAllTickets = async () => {
+    const token = getAuthToken();
+    if (!token) {
+        return null;
+    }
+
+    const response = await axios.get(`${baseURI}`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        }
+    });
+    return response.data;
+}
