c21c37309544b6521bc048875a1dbdd3c9ff1c12
Pruthviraj Deshmukh <raj@Pruthvirajs-MacBook-Pro.local>
2026-02-27 15:16:39 +0530

    feat: add protected routes for authenticated pages

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -3,12 +3,35 @@ import Home from "../pages/Home";
 import Signup from "../pages/Signup";
 import Login from "../pages/Login";
 import NotFound from "../pages/NotFound";
+import ProtectedRoute from "./ProtectedRoute";
+import CreateTicket from "../pages/CreateTicket";
+import ViewTicket from "../pages/ViewTicket";
+import Tickets from "../pages/Tickets";
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route index path="/" element={<Home />} />
+                <Route path="/" element={
+                    <ProtectedRoute>
+                        <Home />
+                    </ProtectedRoute>
+                } />
+                <Route path="/tickets/create" element={
+                    <ProtectedRoute>
+                        <CreateTicket />
+                    </ProtectedRoute>
+                } />
+                <Route path="/tickets" element={
+                    <ProtectedRoute>
+                        <Tickets />
+                    </ProtectedRoute>
+                } />
+                <Route path="/tickets/:id" element={
+                    <ProtectedRoute>
+                        <ViewTicket />
+                    </ProtectedRoute>
+                } />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="*" element={<NotFound />} />
---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,14 @@
+import { Navigate } from "react-router-dom";
+import { getAuthToken } from "../utils/authToken";
+
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+    const token = getAuthToken();
+
+    if (!token) {
+        return <Navigate to="/login" replace />;
+    }
+
+    return children;
+};
+
+export default ProtectedRoute;
